c3faeacfa374511332d6774796b6de850d7d5005
zorro901 <zoro907@gmail.com>
2021-08-31 22:43:53 +0900

    エラー再現

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -11,10 +11,11 @@ const createApolloClient = () => {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({
-      uri: process.env.NEXT_PUBLIC_HASURA_URL,
-      headers: {
-        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_KEY,
-      },
+      uri : 'https://basic-lesson-hasura-200.hasura.app/v1/graphql'
+      // uri: process.env.NEXT_PUBLIC_HASURA_URL,
+      // headers: {
+      //   'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_KEY,
+      // },
     }),
     cache: new InMemoryCache(),
   })
